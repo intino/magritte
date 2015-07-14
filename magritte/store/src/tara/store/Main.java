812b89ad0dd7b7ff854a5bd0eba69c839612315a
Jose Juan Hernandez <josejuanhernandez@siani.es>
2015-07-14 12:18:01 +0100

    spark server included

---
+++ magritte/store/src/tara/store/Main.java
@@ -0,0 +1,12 @@
+package tara.store;
+
+import static spark.Spark.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        port(8080);
+        get("/", (req, res) -> "Hello World");
+    }
+
+}
