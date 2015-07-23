127801ba7a813402c88d7735c05af17fcdc807ac
Octavio Roncal <octavioroncal11@gmail.com>
2015-07-23 08:46:12 +0100

    updated stash creation

--- ide/rt/src/tara/compiler/codegeneration/magritte/stash/StashCreator.java
+++ ide/rt/src/tara/compiler/codegeneration/magritte/stash/StashCreator.java
@@ -3,6 +3,7 @@ package tara.compiler.codegeneration.magritte.stash;
 import tara.io.*;
 import tara.language.model.Node;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,51 +26,68 @@ public class StashCreator {
 		return stash;
 	}
 
-	private Type createType(Node container) {
+	private Type createType(Node node) {
 		final Type type = new Type();
-		type.types = container.types().toArray(new String[container.types().size()]);
-		type.allowsMultiple = collectAllowsMultiple(container);
-		type.requiresMultiple = collectRequiresMultiple(container);
-		type.allowsSingle = collectAllowsSingle(container);
-		type.requiresSingle = collectRequiresSingle(container);
-		for (Node node : container.components()) {
-			if (node.isTerminalInstance()) type.add(createCase(node));
-			else if (node.isPrototype()) type.add(createPrototype(node));
-		}
+		type.isAbstract = node.isAbstract();
+		type.name = node.name();
+		type.types = collectTypes(node);
+		List<Node> nodes = collectTypeComponents(node.components());
+		type.allowsMultiple = collectAllowsMultiple(nodes);
+		type.requiresMultiple = collectRequiresMultiple(nodes);
+		type.allowsSingle = collectAllowsSingle(nodes);
+		type.requiresSingle = collectRequiresSingle(nodes);
+		for (Node component : node.components())
+			if (component.isTerminalInstance()) type.add(createCase(component));
+			else if (component.isPrototype()) type.add(createPrototype(component));
 		return type;
 	}
 
-	private String[] collectAllowsMultiple(Node container) {
-		return new String[0];
+	private String[] collectTypes(Node node) {
+		List<String> types = new ArrayList<>();
+		types.add(node.parentName());
+		types.addAll(node.types());
+		return types.toArray(new String[node.types().size()]);
 	}
 
-	private String[] collectRequiresMultiple(Node container) {
-		return new String[0];
+	private List<Node> collectTypeComponents(List<Node> nodes) {
+		return nodes.stream().filter(component -> !component.isTerminalInstance() && component.isPrototype()).collect(Collectors.toList());
 	}
 
-	private String[] collectAllowsSingle(Node container) {
-		return new String[0];
+	private String[] collectAllowsMultiple(List<Node> nodes) {
+		List<String> components = nodes.stream().filter(component -> !component.isRequired() && !component.isSingle()).map(Node::type).collect(Collectors.toList());
+		return components.toArray(new String[components.size()]);
 	}
 
-	private String[] collectRequiresSingle(Node container) {
-		return new String[0];
+	private String[] collectRequiresMultiple(List<Node> nodes) {
+		List<String> components = nodes.stream().filter(component -> component.isRequired() && !component.isSingle()).map(Node::type).collect(Collectors.toList());
+		return components.toArray(new String[components.size()]);
 	}
 
-	private Prototype[] createPrototypes(List<Node> components) {
-		List<Prototype> prototypes = components.stream().map(this::createPrototype).collect(Collectors.toList());
+	private String[] collectAllowsSingle(List<Node> nodes) {
+		List<String> components = nodes.stream().filter(component -> !component.isRequired() && component.isSingle()).map(Node::type).collect(Collectors.toList());
+		return components.toArray(new String[components.size()]);
+	}
+
+	private String[] collectRequiresSingle(List<Node> nodes) {
+		List<String> components = nodes.stream().filter(component -> component.isRequired() && component.isSingle()).map(Node::type).collect(Collectors.toList());
+		return components.toArray(new String[components.size()]);
+	}
+
+	private Prototype[] createPrototypes(List<Node> nodes) {
+		List<Prototype> prototypes = nodes.stream().map(this::createPrototype).collect(Collectors.toList());
 		return prototypes.toArray(new Prototype[prototypes.size()]);
 	}
 
 	private Prototype createPrototype(Node node) {
-		return new Prototype(node.name(), node.types().toArray(new String[node.types().size()]), variablesOf(node), createPrototypes(node.components()));
+		return new Prototype(node.name(), collectTypes(node), variablesOf(node), createPrototypes(node.components()));
 	}
 
-	private List<Case> createCases(List<Node> components) {
-		return components.stream().map(this::createCase).collect(Collectors.toList());
+	private List<Case> createCases(List<Node> nodes) {
+		return nodes.stream().map(this::createCase).collect(Collectors.toList());
 	}
 
 	private Case createCase(Node node) {
-		return new Case(node.name(), node.types().toArray(new String[node.types().size()]), variablesOf(node), createCases(node.components()));
+		return new Case(node.name(), collectTypes(node), variablesOf(node), createCases(node.components()));
 	}
 
 	private Variable[] variablesOf(Node node) {
--- magritte/store/src/tara/io/Type.java
+++ magritte/store/src/tara/io/Type.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Type {
+	public boolean isAbstract;
+	public String name;
 	public String[] types;
 	public String[] allowsMultiple;
 	public String[] allowsSingle;
