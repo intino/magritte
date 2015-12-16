56f22170db372b5c73ea5a9097060cbf692607fd
jevora <jose.evora@siani.es>
2015-12-16 14:23:49 +0000

    Store modified to allow the saving of stashes

--- magritte/false-mirror/src/tara/magritte/stores/FileSystemStore.java
+++ magritte/false-mirror/src/tara/magritte/stores/FileSystemStore.java
@@ -2,11 +2,14 @@ package tara.magritte.stores;
 
 import tara.io.Stash;
 import tara.io.StashDeserializer;
+import tara.io.StashSerializer;
 import tara.magritte.Store;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Files;
 import java.util.logging.Logger;
 
 @SuppressWarnings("unused")
@@ -22,19 +25,40 @@ public class FileSystemStore implements Store {
 
     @Override
     public Stash stashFrom(String path) {
-        File file = new File(this.file, path);
-        if (file.exists()) return StashDeserializer.stashFrom(file);
+		if (fileOf(path).exists()) return StashDeserializer.stashFrom(fileOf(path));
         return new ResourcesStore().stashFrom(path);
     }
 
-    @Override
+	@Override
     public URL resourceFrom(String path) {
         try {
-            File file = new File(this.file, path);
-            return file.exists() ? file.toURI().toURL() : new ResourcesStore().resourceFrom(path);
+			return fileOf(path).exists() ? fileOf(path).toURI().toURL() : new ResourcesStore().resourceFrom(path);
         } catch (MalformedURLException e) {
             LOG.severe(e.getMessage());
             return null;
         }
     }
+
+	@Override
+	public void writeStash(String path, Stash stash) {
+		if(fileOf(path).exists())
+			doWriteStash(path, composeStash(path, stash));
+		else if(new ResourcesStore().stashFrom(path) != null)
+			new ResourcesStore().writeStash(path, stash);
+		else
+			doWriteStash(path, stash);
+	}
+
+	private File fileOf(String path) {
+		return new File(this.file, path);
+	}
+
+	private void doWriteStash(String path, Stash stash) {
+		try {
+			Files.write(fileOf(path).toPath(), StashSerializer.serialize(stash));
+		} catch (IOException e) {
+			LOG.severe("File at " + path + " couldn't be written");
+		}
+	}
+
 }
--- magritte/false-mirror/src/tara/magritte/stores/ResourcesStore.java
+++ magritte/false-mirror/src/tara/magritte/stores/ResourcesStore.java
@@ -2,27 +2,68 @@ package tara.magritte.stores;
 
 import tara.io.Stash;
 import tara.io.StashDeserializer;
+import tara.io.StashSerializer;
 import tara.magritte.Store;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URL;
+import java.net.URLConnection;
+import java.util.logging.Logger;
 
 public class ResourcesStore implements Store {
 
-    @Override
-    public Stash stashFrom(String path) {
-        InputStream stream = ResourcesStore.class.getResourceAsStream(getPath(path));
-        if (stream == null) return null;
-        return StashDeserializer.stashFrom(stream);
-    }
+	private static final Logger LOG = Logger.getLogger(ResourcesStore.class.getName());
 
-    @Override
-    public URL resourceFrom(String path) {
-        return ResourcesStore.class.getResource(getPath(path));
-    }
+	@Override
+	public Stash stashFrom(String path) {
+		InputStream stream = ResourcesStore.class.getResourceAsStream(getPath(path));
+		if (stream == null) return null;
+		return StashDeserializer.stashFrom(stream);
+	}
 
-    private String getPath(String path) {
-        return path.startsWith("/") ? path : "/" + path;
-    }
+	@Override
+	public URL resourceFrom(String path) {
+		return ResourcesStore.class.getResource(getPath(path));
+	}
+
+
+	@Override
+	public void writeStash(String path, Stash stash) {
+		if(ResourcesStore.class.getResourceAsStream(getPath(path)) != null)
+			doWriteStash(path, stash);
+		else
+			doWriteStash(path, composeStash(path, stash));
+	}
+
+	private OutputStream getOutputStreamOf(String path) {
+		try {
+			URLConnection urlConnection = ResourcesStore.class.getResource(getPath(path)).openConnection();
+			urlConnection.setDoOutput(true);
+			return urlConnection.getOutputStream();
+		} catch (IOException e) {
+			return null;
+		}
+	}
+
+	private void doWriteStash(String path, Stash stash) {
+		OutputStream outputStream = getOutputStreamOf(path);
+		if (outputStream == null) LOG.severe("Resource at " + path + " couldn't be written");
+		else doWriteStash(outputStream, stash);
+	}
+
+	private void doWriteStash(OutputStream outputStream, Stash stash) {
+		try {
+			outputStream.write(StashSerializer.serialize(stash));
+			outputStream.close();
+		} catch (IOException e) {
+			LOG.severe(e.getCause().getMessage());
+		}
+	}
+
+	private String getPath(String path) {
+		return path.startsWith("/") ? path : "/" + path;
+	}
 
 }
