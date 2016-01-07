60f387745a11d4e14762c5d189ee0cac15c9b221
jevora <jose.evora@siani.es>
2016-01-07 13:58:25 +0000

    Added model wrapper

---
+++ magritte/false-mirror/src/tara/magritte/ModelWrapper.java
@@ -0,0 +1,8 @@
+package tara.magritte;
+
+public abstract class ModelWrapper {
+
+	protected abstract void addInstance(Instance instance);
+	protected abstract void removeInstance(Instance instance);
+
+}
