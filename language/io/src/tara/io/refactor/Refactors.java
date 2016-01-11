e000ca589f3d8e5993b8e09622acb0d5597d3e98
jevora <jose.evora@siani.es>
2016-01-11 14:32:09 +0000

    Removed id;

--- language/io/src/tara/io/refactor/Refactors.java
+++ language/io/src/tara/io/refactor/Refactors.java
@@ -6,16 +6,12 @@ import java.util.List;
 public class Refactors extends ArrayList<Refactors.Refactor> {
 
 
-	public List<Refactor> subListById(long id) {
-		for (Refactor refactor : this)
-			if (refactor.id >= id)
-				return this.subList(this.indexOf(refactor), this.size());
-		return this;
+	public List<Refactor> subListById(int id) {
+		return id > this.size() ? this : this.subList(id, this.size());
 	}
 
 	public static class Refactor {
 
-		public int id;
 		public String anchor;
 		public String oldQn;
 		public String newQn;
@@ -23,8 +19,7 @@ public class Refactors extends ArrayList<Refactors.Refactor> {
 		public Refactor() {
 		}
 
-		public Refactor(int id, String anchor, String oldQn, String newQn) {
-			this.id = id;
+		public Refactor(String anchor, String oldQn, String newQn) {
 			this.anchor = anchor;
 			this.oldQn = oldQn;
 			this.newQn = newQn;
