2d07d39e4dcf32c2843823e56ca1d57adf53eb2b
Octavio Roncal <octavioroncal11@gmail.com>
2016-01-14 13:14:04 +0000

    Updated layer generation

--- language/builder/gen/tara/templates/layer/NewElementTemplate.java
+++ language/builder/gen/tara/templates/layer/NewElementTemplate.java
@@ -30,7 +30,8 @@ public class NewElementTemplate extends Template {
 			rule().add((condition("type", "Variable & concept & multiple")), not(condition("slot", "values")), not(condition("slot", "wordvalues")), (condition("trigger", "parameters"))).add(literal("java.util.List<tara.magritte.Concept> ")).add(mark("name", "firstLowerCase")),
 			rule().add((condition("type", "Variable & concept")), not(condition("type", "multiple")), not(condition("slot", "values")), not(condition("slot", "wordvalues")), (condition("trigger", "parameters"))).add(literal("tara.magritte.Concept ")).add(mark("name", "firstLowerCase")),
 			rule().add((condition("type", "variable & multiple")), not(condition("slot", "values")), not(condition("slot", "wordvalues")), (condition("trigger", "assign"))).add(literal("newElement._instance().set(newElement, \"")).add(mark("name", "firstLowerCase")).add(literal("\", ")).add(mark("name", "firstLowerCase")).add(literal(");")),
-			rule().add((condition("type", "variable")), not(condition("type", "multiple")), not(condition("slot", "values")), not(condition("slot", "wordvalues")), (condition("trigger", "assign"))).add(literal("newElement._instance().set(newElement, \"")).add(mark("name", "firstLowerCase")).add(literal("\", java.util.Collections.singletonList(")).add(mark("name", "firstLowerCase")).add(literal("));"))
+			rule().add((condition("type", "variable")), not(condition("type", "multiple")), not(condition("slot", "values")), not(condition("slot", "wordvalues")), (condition("trigger", "assign"))).add(literal("newElement._instance().set(newElement, \"")).add(mark("name", "firstLowerCase")).add(literal("\", java.util.Collections.singletonList(")).add(mark("name", "firstLowerCase")).add(literal("));")),
+			rule().add((condition("type", "variable")), not(condition("slot", "values")), not(condition("slot", "wordvalues")), (condition("trigger", "name"))).add(mark("name"))
 		);
 		return this;
 	}
