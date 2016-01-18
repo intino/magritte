48a002c124e658ddd80d932e89cd9a3de2922751
Octavio Roncal <octavioroncal11@gmail.com>
2016-01-18 10:50:59 +0000

    updated layer generation

--- language/builder/gen/tara/templates/dynamicLayer/Getters_referenceTemplate.java
+++ language/builder/gen/tara/templates/dynamicLayer/Getters_referenceTemplate.java
@@ -1,10 +1,11 @@
 package tara.templates.dynamicLayer;
 
-import org.siani.itrules.*;
+import org.siani.itrules.LineSeparator;
+import org.siani.itrules.Template;
 
 import java.util.Locale;
 
-import static org.siani.itrules.LineSeparator.*;
+import static org.siani.itrules.LineSeparator.LF;
 
 public class Getters_referenceTemplate extends Template {
 
@@ -18,7 +19,7 @@ public class Getters_referenceTemplate extends Template {
 
 	public Template define() {
 		add(
-			rule().add((condition("type", "Variable & reference")), not(condition("type", "concept")), (condition("type", "multiple")), not(condition("type", "inherited")), not(condition("type", "overriden")), (condition("trigger", "getter"))).add(literal("public List<")).add(mark("type", "variableType")).add(literal("> ")).add(mark("name", "firstLowerCase")).add(literal("() {\n\treturn ")).add(mark("name", "firtLowercase")).add(literal(".stream().map(r -> r.instance() == null ? null : r.instance().as(")).add(mark("type", "variableType")).add(literal(".class)).collect(java.util.stream.Collectors.toList());\n}")),
+			rule().add((condition("type", "Variable & reference")), not(condition("type", "concept")), (condition("type", "multiple")), not(condition("type", "inherited")), not(condition("type", "overriden")), (condition("trigger", "getter"))).add(literal("public List<")).add(mark("type", "variableType")).add(literal("> ")).add(mark("name", "firstLowerCase")).add(literal("() {\n\treturn new tara.magritte.utils.ReferenceList<>(")).add(mark("name", "firtLowercase")).add(literal(", ")).add(mark("type", "variableType")).add(literal(".class);\n}")),
 			rule().add((condition("type", "Variable & reference")), not(condition("type", "concept")), not(condition("type", "inherited")), not(condition("type", "overriden")), (condition("trigger", "getter"))).add(literal("public ")).add(mark("type", "variableType")).add(literal(" ")).add(mark("name", "firstLowerCase")).add(literal("() {\n\treturn ")).add(mark("name", "firtLowercase")).add(literal(".instance() == null ? null : ")).add(mark("name", "firtLowercase")).add(literal(".instance().as(")).add(mark("type", "reference")).add(literal(".class);\n}"))
 		);
 		return this;
