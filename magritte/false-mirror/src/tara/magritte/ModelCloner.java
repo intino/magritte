9adcb3f7aa9f443805c107959e5d6215a3eb03c7
jevora <jose.evora@siani.es>
2016-01-19 13:33:19 +0000

    Fixed bug. Engine and Domain were not cloned.

--- magritte/false-mirror/src/tara/magritte/ModelCloner.java
+++ magritte/false-mirror/src/tara/magritte/ModelCloner.java
@@ -9,14 +9,13 @@ public class ModelCloner {
     public static Model doClone(Model model, Model clone) {
         clone.loaders = new ArrayList<>(model.loaders);
         clone.languages = new HashSet<>(model.languages);
-        clone.engine = model.engine;
-        clone.domain = model.domain;
         clone.concepts = new HashMap<>(model.concepts);
-        clone.instances = new HashMap<>(model.instances);
-        clone.layerFactory = model.layerFactory;
+		clone.instances = new HashMap<>(model.instances);
+		clone.layerFactory = model.layerFactory;
 		clone.openedStashes = new HashSet<>(model.openedStashes);
-        model.soil.components().forEach(clone.soil::add);
-        return clone;
+		model.soil.components().forEach(clone.soil::add);
+		clone.init(model.domain.getClass(), model.engine.getClass());
+		return clone;
     }
 
 }
