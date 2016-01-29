783e6ae52ea33509b37d3b44187c3c972b0d68aa
Octavio Roncal <octavioroncal11@gmail.com>
2016-01-29 12:46:39 +0000

    Updated templates.

--- language/builder/gen/tara/templates/EngineTemplate.java
+++ language/builder/gen/tara/templates/EngineTemplate.java
@@ -1,11 +1,10 @@
 package tara.templates;
 
-import org.siani.itrules.LineSeparator;
-import org.siani.itrules.Template;
+import org.siani.itrules.*;
 
 import java.util.Locale;
 
-import static org.siani.itrules.LineSeparator.LF;
+import static org.siani.itrules.LineSeparator.*;
 
 public class EngineTemplate extends Template {
 
@@ -19,7 +18,7 @@ public class EngineTemplate extends Template {
 
 	public Template define() {
 		add(
-			rule().add((condition("type", "engine"))).add(literal("package ")).add(mark("generatedLanguage", "lowercase")).add(literal(";\n\nimport tara.magritte.Model;\n\npublic class ")).add(mark("generatedLanguage")).add(literal("Engine extends ")).add(mark("generatedLanguage", "lowercase")).add(literal(".ModelWrapper implements tara.magritte.Engine {\n\n\tpublic ")).add(mark("generatedLanguage")).add(literal("Engine(Model model) {\n\t\tsuper(model);\n\t}\n\n\tpublic void init() {\n\t\t// Insert init code here\n\t}\n\n\tpublic void execute() {\n\t\t// Insert execute code here\n\t}\n}"))
+			rule().add((condition("type", "engine"))).add(literal("package ")).add(mark("generatedLanguage", "lowercase")).add(literal(";\n\nimport tara.magritte.Model;\n\npublic class ")).add(mark("generatedLanguage")).add(literal("Engine extends ")).add(mark("generatedLanguage", "lowercase")).add(literal(".ModelWrapper implements tara.magritte.Engine {\n\n\tpublic ")).add(mark("generatedLanguage")).add(literal("Engine(Model model) {\n\t\tsuper(model);\n\t\t_model.messageProvider().register(\"")).add(mark("generatedLanguage")).add(literal("\");\n\t}\n\n\tpublic void init() {\n\t\t// Insert init code here\n\t}\n\n\tpublic void execute() {\n\t\t// Insert execute code here\n\t}\n}"))
 		);
 		return this;
 	}
--- magritte/false-mirror/src/tara/magritte/utils/MessageProvider.java
+++ magritte/false-mirror/src/tara/magritte/utils/MessageProvider.java
@@ -1,26 +1,58 @@
 package tara.magritte.utils;
 
+import tara.magritte.ModelHandler;
+
 import java.text.MessageFormat;
 import java.util.*;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 import static java.util.ResourceBundle.getBundle;
 
 public class MessageProvider {
+	protected static final Logger LOG = Logger.getLogger(ModelHandler.class.getName());
 
 	private static String BUNDLE = "messages.";
-	private Map<String, PropertyResourceBundle> resourceBundle = new HashMap<>();
+	private List<String> levels = new ArrayList<>();
+	private Map<String, List<PropertyResourceBundle>> bundles = new HashMap<>();
 
-	public String message(String level, String locale, String key, Object... params) {
-		if (!resourceBundle.containsKey(locale))
-			resourceBundle.put(locale, (PropertyResourceBundle) getBundle(BUNDLE + level, new Locale(locale, "")));
+	public void register(String level) {
+		levels.add(0, level);
+	}
+
+	public String message(String locale, String key, Object... params) {
+		if (!bundles.containsKey(locale)) bundles.put(locale, collectBundles(locale));
 		return resolveMessage(locale, key, params);
 	}
 
+	public Map<String, String> wordsIn(String language) {
+		Map<String, String> map = new HashMap();
+		extractKeys(language, map);
+		return map;
+	}
+
+	private List<PropertyResourceBundle> collectBundles(String locale) {
+		return levels.stream().map(level -> (PropertyResourceBundle) getBundle(BUNDLE + level, new Locale(locale, ""))).collect(Collectors.toList());
+	}
+
 	private String resolveMessage(String locale, String key, Object[] params) {
-		try {
-			return MessageFormat.format(resourceBundle.get(locale).getString(key), params);
-		} catch (MissingResourceException e) {
-			return key;
+		for (PropertyResourceBundle bundle : bundles.get(locale)) {
+			try {
+				return MessageFormat.format(bundle.getString(key), params);
+			} catch (MissingResourceException ignored) {
+			}
+		}
+		LOG.warning("Message key not found");
+		return key;
+	}
+
+	private void extractKeys(String language, Map<String, String> map) {
+		for (PropertyResourceBundle bundle : bundles.get(language)) {
+			final Enumeration<String> keys = bundle.getKeys();
+			while (keys.hasMoreElements()) {
+				final String key = keys.nextElement();
+				map.put(key, bundle.getString(key));
+			}
 		}
 	}
 
