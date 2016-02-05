6b7837733f5c768331d527b156ec3ab660fbe611
Octavio Roncal <octavioroncal11@gmail.com>
2016-02-05 08:34:50 +0000

    retina support

--- ide/plugin/src/tara/intellij/lang/TaraIcons.java
+++ ide/plugin/src/tara/intellij/lang/TaraIcons.java
@@ -1,26 +1,25 @@
 package tara.intellij.lang;
 
-import com.intellij.openapi.util.IconLoader;
+import com.intellij.util.IconUtil;
+import com.intellij.util.ui.UIUtil;
 
 import javax.swing.*;
-import java.awt.*;
+
+import static com.intellij.openapi.util.IconLoader.getIcon;
 
 public class TaraIcons {
 
+	private static final boolean RETINA = UIUtil.isRetina();
 
-	private static final boolean RETINA = retina();
-
-	public static final Icon ICON_16 = IconLoader.getIcon(RETINA ? "/icons/icon_retina.png" : "/icons/icon.png");
-	public static final Icon LOGO_16 = IconLoader.getIcon(RETINA ? "/icons/logo_retina.png" : "/icons/logo_16.png");
-	public static final Icon NODE = IconLoader.getIcon(RETINA ? "/icons/icon_retina.png" : "/icons/icon.png");
-	public static final Icon STASH_16 = IconLoader.getIcon(RETINA ? "/icons/stash_retina.png" : "/icons/stash_16.png");
+	public static final Icon ICON_16 = RETINA ? scale(getIcon("/icons/icon_retina.png")) : getIcon("/icons/icon.png");
+	public static final Icon LOGO_16 = RETINA ? scale(getIcon("/icons/logo_retina.png")) : getIcon("/icons/logo_16.png");
+	public static final Icon NODE = RETINA ? scale(getIcon("/icons/icon_retina.png")) : getIcon("/icons/icon.png");
+	public static final Icon STASH_16 = RETINA ? scale(getIcon("/icons/stash_retina.png")) : getIcon("/icons/stash_16.png");
 
 	private TaraIcons() {
 	}
 
-	private static boolean retina() {
-		Object obj = Toolkit.getDefaultToolkit().getDesktopProperty("apple.awt.contentScaleFactor");
-		return obj instanceof Float && (((Float) obj).intValue() == 2);
+	private static Icon scale(Icon icon) {
+		return IconUtil.scale(icon, 0.5);
 	}
-
 }
