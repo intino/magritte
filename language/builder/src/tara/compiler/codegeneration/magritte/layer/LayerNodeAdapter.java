4069b3d0cb0ef2b852153ebf2e93067f0b4bde81
Octavio Roncal <octavioroncal11@gmail.com>
2016-04-15 08:02:23 +0100

    Updating layer api

--- language/builder/src/tara/compiler/codegeneration/magritte/layer/LayerNodeAdapter.java
+++ language/builder/src/tara/compiler/codegeneration/magritte/layer/LayerNodeAdapter.java
@@ -26,6 +26,7 @@ import static tara.lang.model.Tag.Instance;
 
 class LayerNodeAdapter extends Generator implements Adapter<Node>, TemplateTags {
 	private static final Logger LOG = Logger.getLogger(ParseOperation.class.getName());
+	private static final String PARENT_SUPER = "parentSuper";
 	private Node initNode;
 	private FrameContext context;
 	private final int level;
@@ -66,6 +67,7 @@ class LayerNodeAdapter extends Generator implements Adapter<Node>, TemplateTags 
 		node.flags().stream().filter(isLayerInterface()).forEach(tag -> frame.addFrame(FLAG, tag));
 		if (node.isTerminal()) frame.addFrame(FLAG, Tag.Concept);
 		if (node.parent() != null) frame.addTypes(CHILD);
+		frame.addFrame(PARENT_SUPER, node.parent() != null);
 		if (node.components().stream().filter(c -> c.is(Instance)).findFirst().isPresent())
 			frame.addFrame(META_TYPE, language.languageName().toLowerCase() + DOT + metaType(node));
 		addVariables(frame, node);
