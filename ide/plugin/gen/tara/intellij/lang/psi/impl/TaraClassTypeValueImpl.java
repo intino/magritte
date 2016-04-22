d1b8acd1406a59f882215d29c1f03fdf7d942f69
Octavio Roncal <octavioroncal11@gmail.com>
2016-04-22 07:17:55 +0100

    Added templates and gen classes

---
+++ ide/plugin/gen/tara/intellij/lang/psi/impl/TaraClassTypeValueImpl.java
@@ -0,0 +1,29 @@
+// This is a generated file. Not intended for manual editing.
+package tara.intellij.lang.psi.impl;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.util.PsiTreeUtil;
+import static tara.intellij.lang.psi.TaraTypes.*;
+import com.intellij.extapi.psi.ASTWrapperPsiElement;
+import tara.intellij.lang.psi.*;
+
+public class TaraClassTypeValueImpl extends ASTWrapperPsiElement implements TaraClassTypeValue {
+
+  public TaraClassTypeValueImpl(ASTNode node) {
+    super(node);
+  }
+
+  public void accept(@NotNull TaraVisitor visitor) {
+    visitor.visitClassTypeValue(this);
+  }
+
+  public void accept(@NotNull PsiElementVisitor visitor) {
+    if (visitor instanceof TaraVisitor) accept((TaraVisitor)visitor);
+    else super.accept(visitor);
+  }
+
+}
