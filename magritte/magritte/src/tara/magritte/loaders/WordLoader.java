a34ff63a32222ae854fdf1de064cd719b20d4bc8
Octavio Roncal <octavioroncal@monentia.es>
2016-09-06 14:40:21 +0100

    updated loaders

--- magritte/magritte/src/tara/magritte/loaders/WordLoader.java
+++ magritte/magritte/src/tara/magritte/loaders/WordLoader.java
@@ -10,13 +10,13 @@ import static tara.magritte.loaders.ListProcessor.process;
 @SuppressWarnings("unused")
 public class WordLoader {
 
-    public static <T extends Enum<T>> List<T> load(List<?> list, Class<T> aClass, Layer layer) {
-        return list.stream().map(w -> processWord(aClass, (String)w, layer)).collect(toList());
-    }
+	public static <T extends Enum<T>> List<T> load(List<?> list, Class<T> aClass, Layer layer) {
+		return list.stream().map(w -> processWord(aClass, (String) w, layer)).collect(toList());
+	}
 
-    private static <T extends Enum<T>> T processWord(Class<T> aClass, String word, Layer layer) {
-        Object wordObject = process((Object)word, layer);
-        return aClass.isInstance(wordObject) ? (T) wordObject : Enum.valueOf(aClass, word);
-    }
+	private static <T extends Enum<T>> T processWord(Class<T> aClass, String word, Layer layer) {
+		Object wordObject = process((Object) word, layer);
+		return aClass.isInstance(wordObject) ? (T) wordObject : Enum.valueOf(aClass, word);
+	}
 
 }
