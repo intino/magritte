45fb0d5e605c17ea2659e1ee6e41adb6ffaa933e
Octavio Roncal <octavioroncal@monentia.es>
2016-09-21 13:12:04 +0100

    moved to teseo

---
+++ ide/teseo/application/gen/teseo/natives/date/data/Type_0.java
@@ -0,0 +1,23 @@
+package teseo.natives.date.data;
+
+
+
+/**#/Users/oroncal/workspace/tara/ide/liskov/application/src/teseo/Teseo.tara#37#2**/
+public class Type_0 implements tara.magritte.Expression<String> {
+	private teseo.date.DateData self;
+
+	@Override
+	public String value() {
+		return "java.time.LocalDate";
+	}
+
+	@Override
+	public void self(tara.magritte.Layer context) {
+		self = (teseo.date.DateData) context;
+	}
+
+	@Override
+	public Class<? extends tara.magritte.Layer> selfClass() {
+		return teseo.date.DateData.class;
+	}
+}
---
+++ ide/teseo/application/gen/teseo/natives/integer/data/Type_0.java
@@ -0,0 +1,23 @@
+package teseo.natives.integer.data;
+
+
+
+/**#/Users/oroncal/workspace/tara/ide/liskov/application/src/teseo/Teseo.tara#28#2**/
+public class Type_0 implements tara.magritte.Expression<String> {
+	private teseo.integer.IntegerData self;
+
+	@Override
+	public String value() {
+		return "Integer";
+	}
+
+	@Override
+	public void self(tara.magritte.Layer context) {
+		self = (teseo.integer.IntegerData) context;
+	}
+
+	@Override
+	public Class<? extends tara.magritte.Layer> selfClass() {
+		return teseo.integer.IntegerData.class;
+	}
+}
