5ea1920fd4a0b4bffac533a00c25f048e7110b5d
Jose Évora <jose.evora@siani.es>
2016-09-22 12:15:02 +0100

    legio updated

---
+++ ide/legio/legio-core/src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static List<String> calculate(legio.Project self) {
+        return Collections.emptyList();
+    }
+}
