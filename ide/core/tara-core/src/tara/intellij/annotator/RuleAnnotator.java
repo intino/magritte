1626f309b1bfb7b27d2cff7f416d9ca47f571a3a
Octavio Roncal <octavioroncal@monentia.es>
2016-11-11 09:22:55 +0000

    added analyzer for rules over instances

--- ide/core/tara-core/src/tara/intellij/annotator/RuleAnnotator.java
+++ ide/core/tara-core/src/tara/intellij/annotator/RuleAnnotator.java
@@ -3,7 +3,7 @@ package tara.intellij.annotator;
 import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.psi.PsiElement;
 import org.jetbrains.annotations.NotNull;
-import tara.intellij.annotator.semanticanalizer.NodeRuleClassAnalyzer;
+import tara.intellij.annotator.semanticanalizer.NodeRuleAnalyzer;
 import tara.intellij.annotator.semanticanalizer.VariableRuleClassAnalyzer;
 import tara.intellij.lang.psi.TaraRuleContainer;
 import tara.intellij.lang.psi.impl.TaraPsiImplUtil;
@@ -20,7 +20,6 @@ public class RuleAnnotator extends TaraAnnotator {
 		if (!(element instanceof TaraRuleContainer) || (variable != null && OBJECT.equals(variable.type()))) return;
 		TaraRuleContainer ruleContainer = (TaraRuleContainer) element;
 		analyzeAndAnnotate(TaraPsiImplUtil.getContainerByType(ruleContainer, Variable.class) != null ?
-			new VariableRuleClassAnalyzer(ruleContainer) :
-			new NodeRuleClassAnalyzer(ruleContainer));
+			new VariableRuleClassAnalyzer(ruleContainer) : new NodeRuleAnalyzer(ruleContainer));
 	}
 }
--- ide/core/tara-core/src/tara/intellij/annotator/semanticanalizer/NodeRuleAnalyzer.java
+++ ide/core/tara-core/src/tara/intellij/annotator/semanticanalizer/NodeRuleAnalyzer.java
@@ -5,7 +5,6 @@ import com.intellij.openapi.module.Module;
 import com.intellij.psi.JavaPsiFacade;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiElement;
-import tara.dsl.ProteoConstants;
 import tara.intellij.annotator.TaraAnnotator;
 import tara.intellij.annotator.fix.CreateNodeRuleClassIntention;
 import tara.intellij.codeinsight.languageinjection.helpers.Format;
@@ -17,27 +16,24 @@ import tara.intellij.messages.MessageProvider;
 import tara.intellij.project.TaraModuleType;
 import tara.intellij.project.module.ModuleProvider;
 import tara.lang.model.Node;
+import tara.lang.model.Tag;
 import tara.lang.model.rules.custom.Url;
 
 import static com.intellij.psi.search.GlobalSearchScope.moduleScope;
 import static tara.intellij.highlighting.TaraSyntaxHighlighter.UNRESOLVED_ACCESS;
 import static tara.lang.semantics.errorcollector.SemanticNotification.Level.ERROR;
 
-public class NodeRuleClassAnalyzer extends TaraAnalyzer {
+public class NodeRuleAnalyzer extends TaraAnalyzer {
 
 	private static final String RULES_PACKAGE = ".rules.";
 	private final String rulesPackage;
 	private final Rule rule;
 	private final Node node;
 
-	public NodeRuleClassAnalyzer(TaraRuleContainer ruleContainer) {
+	public NodeRuleAnalyzer(TaraRuleContainer ruleContainer) {
 		this.node = TaraPsiImplUtil.getContainerByType(ruleContainer, Node.class);
 		this.rule = ruleContainer.getRule();
 		rulesPackage = (TaraModuleType.isTara(module()) ? TaraUtil.workingPackage(ruleContainer) : "").toLowerCase() + RULES_PACKAGE;
-	}
-
-	public boolean isFacet() {
-		return node != null && ProteoConstants.FACET.equals(node.type());
 	}
 
 	@Override
@@ -45,11 +41,18 @@ public class NodeRuleClassAnalyzer extends TaraAnalyzer {
 		if (rule == null) {
 			error();
 			return;
+		} else if (node.is(Tag.Instance)) {
+			instanceError();
+			return;
 		}
 		final Module module = module();
 		if (rule.isLambda() || module == null) return;
 		PsiClass aClass = JavaPsiFacade.getInstance(rule.getProject()).findClass(rulesPackage + rule.getText(), moduleScope(module));
 		if (aClass == null && !isProvided()) error();
+	}
+
+	private void instanceError() {
+		results.put(rule, new TaraAnnotator.AnnotateAndFix(ERROR, MessageProvider.message("instance.node.with.rule"), UNRESOLVED_ACCESS, collectFixes()));
 	}
 
 	private boolean isProvided() {
