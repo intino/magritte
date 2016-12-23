80a0919b248d71b6bc9e4bfdbf86ac84f7c10d55
Jose Évora <jose.evora@siani.es>
2016-12-23 15:03:17 +0000

    -

--- magritte/magritte/src/tara/magritte/loaders/InstantLoader.java
+++ magritte/magritte/src/tara/magritte/loaders/InstantLoader.java
@@ -1,45 +1,32 @@
 package tara.magritte.loaders;
 
 import tara.magritte.Layer;
+import tara.magritte.MagritteException;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static java.util.Arrays.asList;
-
 @SuppressWarnings("unused")
-public class DateLoader {
+public class InstantLoader {
 
-	private static final DateTimeFormatter[] dateFormats = new DateTimeFormatter[20];
-
-	static {
-		String[] patterns = {"dd/MM/yyyy HH:mm:ss", "dd/MM/yyyy HH:mm", "dd/MM/yyyy HH", "dd/MM/yyyy", "MM/yyyy", "yyyy"};
-		asList(patterns).forEach(p -> dateFormats[p.length()] = DateTimeFormatter.ofPattern(p));
+	public static List<Instant> load(List<?> instants, Layer layer) {
+		return instants.stream().map((date) -> processInstant((String) date, layer)).collect(Collectors.toList());
 	}
 
-	public static List<LocalDateTime> load(List<?> dates, Layer layer) {
-		return dates.stream().map((date) -> processDate((String) date, layer)).collect(Collectors.toList());
+	private static Instant processInstant(String instant, Layer layer) {
+		if (instant.isEmpty()) return null;
+		Object dateObject = ListProcessor.process((Object) instant, layer);
+		return dateObject instanceof Instant ? (Instant) dateObject : parseInstant(instant);
 	}
 
-	private static LocalDateTime processDate(String date, Layer layer) {
-		if (date.isEmpty()) return null;
-		Object dateObject = ListProcessor.process((Object) date, layer);
-		return dateObject instanceof LocalDateTime ? (LocalDateTime) dateObject : parseDate(date);
-	}
-
-	private static LocalDateTime parseDate(String date) {
-		date = addTime(date);
-		if (date.length() < dateFormats.length && dateFormats[date.length()] != null)
-			return LocalDateTime.from(dateFormats[date.length()].parse(date));
-		throw new RuntimeException("Date couldn't be parsed: " + date);
-	}
-
-	private static String addTime(String date) {
-		return date.length() == 10 ? date + " 00" :
-			date.length() == 7 ? "01/" + date + " 00" :
-				date.length() == 4 ? "01/01/" + date + " 00" : date;
-	}
+	private static Instant parseInstant(String instant) {
+        try {
+            return Instant.parse(instant);
+        } catch (Exception e) {
+            throw new MagritteException(e.getMessage());
+        }
+    }
 
 }
