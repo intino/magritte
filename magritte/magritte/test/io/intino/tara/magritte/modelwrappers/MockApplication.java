05ee790e07fd7cde0e5db4372ea0382be2afdc06
Jose Évora <jose.evora@siani.es>
2017-02-01 12:02:25 +0000

    removing implements

--- magritte/magritte/test/io/intino/tara/magritte/modelwrappers/MockApplication.java
+++ magritte/magritte/test/io/intino/tara/magritte/modelwrappers/MockApplication.java
@@ -1,9 +1,8 @@
 package io.intino.tara.magritte.modelwrappers;
 
-import io.intino.tara.magritte.Application;
 import io.intino.tara.magritte.Graph;
 
-public class MockApplication extends MockPlatform implements Application {
+public class MockApplication extends MockPlatform {
 	public MockApplication(Graph graph) {
 		super(graph);
 	}
