70eb024300b16ec6ad8060aa567ec6a987604d46
Octavio Roncal <octavioroncal@monentia.es>
2017-05-19 14:57:22 +0100

    updated code generation

--- builder/builder/src/io/intino/tara/compiler/codegeneration/magritte/layer/GraphViewTemplate.java
+++ builder/builder/src/io/intino/tara/compiler/codegeneration/magritte/layer/GraphViewTemplate.java
@@ -9,19 +9,19 @@ import java.util.Locale;
 
 import static org.siani.itrules.LineSeparator.LF;
 
-public class GraphWrapperTemplate extends Template {
+public class GraphViewTemplate extends Template {
 
-	protected GraphWrapperTemplate(Locale locale, LineSeparator separator) {
+	protected GraphViewTemplate(Locale locale, LineSeparator separator) {
 		super(locale, separator);
 	}
 
 	public static Template create() {
-		return new GraphWrapperTemplate(Locale.ENGLISH, LF).define();
+		return new GraphViewTemplate(Locale.ENGLISH, LF).define();
 	}
 
 	public Template define() {
 		add(NewElementTemplate.create().rules());
-		add(io.intino.tara.compiler.codegeneration.magritte.layer.templates.layer.GraphWrapperTemplate.create().rules());
+		add(io.intino.tara.compiler.codegeneration.magritte.layer.templates.layer.GraphViewTemplate.create().rules());
 		add(HelpersTemplate.create().rules());
 		return this;
 	}
