a91374fe0c5c5f5a391547fb6619bfecd4bfa39d
Octavio Roncal <octavioroncal@monentia.es>
2017-05-22 10:18:07 +0100

    updated builder templates.

--- builder/builder/src/io/intino/tara/compiler/codegeneration/magritte/layer/GraphTemplate.java
+++ builder/builder/src/io/intino/tara/compiler/codegeneration/magritte/layer/GraphTemplate.java
@@ -1,5 +1,6 @@
 package io.intino.tara.compiler.codegeneration.magritte.layer;
 
+import io.intino.tara.compiler.codegeneration.magritte.layer.templates.layer.AbstractGraphTemplate;
 import org.siani.itrules.LineSeparator;
 import org.siani.itrules.Template;
 import io.intino.tara.compiler.codegeneration.magritte.layer.templates.layer.HelpersTemplate;
@@ -9,19 +10,19 @@ import java.util.Locale;
 
 import static org.siani.itrules.LineSeparator.LF;
 
-public class GraphViewTemplate extends Template {
+public class GraphTemplate extends Template {
 
-	protected GraphViewTemplate(Locale locale, LineSeparator separator) {
+	protected GraphTemplate(Locale locale, LineSeparator separator) {
 		super(locale, separator);
 	}
 
 	public static Template create() {
-		return new GraphViewTemplate(Locale.ENGLISH, LF).define();
+		return new GraphTemplate(Locale.ENGLISH, LF).define();
 	}
 
 	public Template define() {
 		add(NewElementTemplate.create().rules());
-		add(io.intino.tara.compiler.codegeneration.magritte.layer.templates.layer.GraphViewTemplate.create().rules());
+		add(AbstractGraphTemplate.create().rules());
 		add(HelpersTemplate.create().rules());
 		return this;
 	}
