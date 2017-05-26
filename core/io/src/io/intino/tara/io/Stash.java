ea9ed1b1075ce362e4431519ede8322df1c62e5c
Jose Évora <jose.evora@siani.es>
2017-05-26 08:19:18 +0100

    added origin to stash

--- core/io/src/io/intino/tara/io/Stash.java
+++ core/io/src/io/intino/tara/io/Stash.java
@@ -3,13 +3,19 @@ package io.intino.tara.io;
 import java.util.ArrayList;
 import java.util.List;
 
+import static io.intino.tara.io.Stash.Origin.Runtime;
+
 public class Stash {
 	public String language;
 	public String path;
+	public Origin origin = Runtime;
 	public String builder;
-	public String version;
 	public List<String> uses = new ArrayList<>();
 	public List<Concept.Content> contentRules = new ArrayList<>();
 	public List<Concept> concepts = new ArrayList<>();
 	public List<Node> nodes = new ArrayList<>();
+
+	public enum Origin{
+	    Platform, Product, Solution, Runtime
+    }
 }
