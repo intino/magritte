44bd17470d7f3de7f31348e4dc17b927b6790168
Jose Évora <jose.evora@siani.es>
2017-08-04 08:30:35 +0100

    added backdoor to a not existent copy constructor on ~Graph

--- magritte/magritte/src/io/intino/tara/magritte/GraphCloner.java
+++ magritte/magritte/src/io/intino/tara/magritte/GraphCloner.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
+import java.util.logging.Logger;
 
 class GraphCloner {
 
@@ -15,8 +16,18 @@ class GraphCloner {
 		clone.layerFactory = new LayerFactory(graph.layerFactory);
 		clone.openedStashes = new HashSet<>(graph.openedStashes);
 		graph.model.componentList().forEach(clone.model::add);
-		graph.wrappers.values().forEach(w -> clone.wrappers.put(w.getClass(), GraphHelper.create(w.getClass(), clone, w)));
+		graph.wrappers.values().forEach(w -> cloneWrapper(clone, w));
 		return clone;
+	}
+
+	private static void cloneWrapper(Graph clone, GraphWrapper w) {
+		Class<? extends GraphWrapper> aClass = w.getClass();
+		try {
+			clone.wrappers.put(aClass, GraphHelper.create(aClass, clone, w));
+		} catch (Throwable e){
+			Logger.getGlobal().info("Copy constructor on " + aClass.getSimpleName() + " not found. To improve performance when cloning, add constructor \"" + aClass.getSimpleName() + "(Graph graph, " + aClass.getSimpleName() + " wrapper)\" calling super (Builder 2.0.8+)");
+			clone.wrappers.put(aClass, GraphHelper.create(aClass, clone));
+		}
 	}
 
 }
