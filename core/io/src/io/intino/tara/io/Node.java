0dcc5cd497e7d29960e80b58448e0691d9cc1607
jevora <jose.evora@siani.es>
2019-05-16 14:34:48 +0100

    fixed node

--- core/io/src/io/intino/tara/io/Node.java
+++ core/io/src/io/intino/tara/io/Node.java
@@ -17,11 +17,18 @@ public class Node {
 
 	@Override
 	public int hashCode() {
+		String name = nameOf(this.name);
 		return Objects.hash(isUUID(name) ? "" : name, facets, variables, nodes);
 	}
 
+	public static String nameOf(String id) {
+		String shortName = id.contains(".") ? id.substring(id.lastIndexOf(".") + 1) : id;
+		shortName = shortName.contains("#") ? shortName.substring(shortName.lastIndexOf("#") + 1) : shortName;
+		shortName = shortName.contains("$") ? shortName.substring(shortName.lastIndexOf("$") + 1) : shortName;
+		return shortName;
+	}
+
 	private static boolean isUUID(String str) {
-		str = str.length() >= 36 ? str.substring(str.length() - 36) : str;
 		return str.length() == 36 && str.charAt(8) == '-' && str.charAt(13) == '-' && str.charAt(18) == '-' && str.charAt(23) == '-';
 	}
 }
