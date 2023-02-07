4800292bbe50509a176192fe9a8a5820314b2454
Octavio Roncal <octavioroncal@monentia.com>
2023-02-07 15:10:42 +0000

    improved node resolution

--- core/language/src/io/intino/magritte/Resolver.java
+++ core/language/src/io/intino/magritte/Resolver.java
@@ -2,12 +2,14 @@ package io.intino.magritte;
 
 import io.intino.magritte.lang.model.Aspect;
 import io.intino.magritte.lang.model.Node;
+import io.intino.magritte.lang.model.Parameter;
 import io.intino.magritte.lang.semantics.Constraint;
 import io.intino.magritte.lang.semantics.Constraint.OneOf;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 public class Resolver {
 	private final Language language;
@@ -29,7 +31,8 @@ public class Resolver {
 		resolve(context(node));
 		List<Constraint> contextConstraints = contextConstraints(node);
 		if (contextConstraints == null) return;
-		for (Constraint constraint : components(contextConstraints)) if (checkComponentConstraint(node, constraint)) return;
+		for (Constraint constraint : components(contextConstraints))
+			if (checkComponentConstraint(node, constraint)) return;
 		metaAspects(contextConstraints).forEach(constraint -> checkMetaAspectConstraint(node, constraint));
 	}
 
@@ -37,7 +40,8 @@ public class Resolver {
 		if (node == null || language == null) return Collections.emptyList();
 		final Node context = context(node);
 		List<Constraint> constraints = context != null && context.type() != null ? language.constraints(context.type()) : null;
-		if (constraints != null && (isComponent(constraints, node) || isMetaAspect(constraints, node))) return constraints;
+		if (constraints != null && (isComponent(constraints, node) || isMetaAspect(constraints, node)))
+			return constraints;
 		return findInAspects(node);
 	}
 
@@ -53,11 +57,11 @@ public class Resolver {
 	}
 
 	private List<Constraint.Component> components(List<Constraint> context) {
-		return context.stream().filter(c -> c instanceof Constraint.Component).map(c -> (Constraint.Component) c).collect(Collectors.toList());
+		return context.stream().filter(c -> c instanceof Constraint.Component).map(c -> (Constraint.Component) c).collect(toList());
 	}
 
 	private List<Constraint.MetaAspect> metaAspects(List<Constraint> context) {
-		return context.stream().filter(c -> c instanceof Constraint.MetaAspect).map(c -> (Constraint.MetaAspect) c).collect(Collectors.toList());
+		return context.stream().filter(c -> c instanceof Constraint.MetaAspect).map(c -> (Constraint.MetaAspect) c).collect(toList());
 	}
 
 	private boolean isOneOf(Constraint.Component allow, String type) {
@@ -74,15 +78,6 @@ public class Resolver {
 		return null;
 	}
 
-	private void resolveAspects(Node node) {
-		for (Aspect aspect : node.appliedAspects()) {
-			Constraint.Aspect constraint = (Constraint.Aspect) language.constraints(node.type()).stream().
-					filter(c -> c instanceof Constraint.Aspect &&
-							simpleType((Constraint.Aspect) c).equals(aspect.type())).findFirst().orElse(null);
-			if (constraint != null) aspect.fullType(constraint.type());
-		}
-	}
-
 	private String simpleType(Constraint.Aspect aspect) {
 		return aspect.type().contains(".") ? aspect.type().substring(aspect.type().lastIndexOf(".") + 1) : aspect.type();
 	}
@@ -94,18 +89,8 @@ public class Resolver {
 	}
 
 	private void checkMetaAspectConstraint(Node node, Constraint.MetaAspect constraint) {
-		if (node.type() != null && shortType(node.type()).equals(shortType(constraint.type()))) node.type(constraint.type());
-	}
-
-	private boolean checkAsComponent(Node node, Constraint.Component allow) {
-		String absoluteType = allow.type();
-		if (node.type() != null && shortType(node.type()).equals(shortType(absoluteType))) {
-			node.type(absoluteType);
-			node.metaTypes(language.types(absoluteType));
-			resolveAspects(node);
-			return true;
-		}
-		return false;
+		if (node.type() != null && shortType(node.type()).equals(shortType(constraint.type())))
+			node.type(constraint.type());
 	}
 
 	private boolean checkAllowOneOf(Node node, Constraint allow) {
@@ -119,6 +104,47 @@ public class Resolver {
 			}
 		}
 		return false;
+	}
+
+	private boolean checkAsComponent(Node node, Constraint.Component allow) {
+		String absoluteType = allow.type();
+		if (node.type() != null && shortType(node.type()).equals(shortType(absoluteType))) {
+			node.type(absoluteType);
+			node.metaTypes(language.types(absoluteType));
+			resolveAspects(node);
+			resolveParameters(node);
+			return true;
+		}
+		return false;
+	}
+
+	private void resolveAspects(Node node) {
+		for (Aspect aspect : node.appliedAspects()) {
+			Constraint.Aspect constraint = (Constraint.Aspect) language.constraints(node.type()).stream().
+					filter(c -> c instanceof Constraint.Aspect && simpleType((Constraint.Aspect) c).equals(aspect.type())).findFirst().orElse(null);
+			if (constraint != null) {
+				aspect.fullType(constraint.type());
+				List<Constraint.Parameter> parameterConstraints = constraint.constraints().stream().filter(c -> c instanceof Constraint.Parameter).map(c -> (Constraint.Parameter) c).collect(toList());
+				resolveParameters(aspect.parameters(), parameterConstraints);
+			}
+		}
+	}
+
+	private void resolveParameters(Node node) {
+		List<Constraint.Parameter> constraints = language.constraints(node.type()).stream()
+				.filter(c -> c instanceof Constraint.Parameter)
+				.map(c -> (Constraint.Parameter) c)
+				.collect(toList());
+		resolveParameters(node.parameters(), constraints);
+	}
+
+	private static void resolveParameters(List<Parameter> parameters, List<Constraint.Parameter> constraints) {
+		for (Parameter param : parameters) {
+			Constraint.Parameter constraint = constraints.stream()
+					.filter(p -> p.name().equals(param.name()) || p.position() == param.position())
+					.findFirst().orElse(null);
+			if (constraint != null && param.name().isEmpty()) param.name(constraint.name());
+		}
 	}
 
 	public Node context(Node node) {
