c1c940dff511cef394fe23a3b6ca4a9c2a9a43f5
jevora <legendus@gmail.com>
2023-03-08 07:49:56 +0000

    improved magritte framework to be more thread-safe

--- magritte/framework/src/io/intino/magritte/framework/GraphHelper.java
+++ magritte/framework/src/io/intino/magritte/framework/GraphHelper.java
@@ -46,7 +46,8 @@ class GraphHelper {
 		Map<String, Node> stashMap = graph.nodes.get(stash);
 		if(stashMap == null) return Collections.emptyList();
 		List<Node> nodes = new ArrayList<>();
-		for (Node node : new ArrayList<>(stashMap.values())) if (node.isRoot()) nodes.add(node);
+		List<Node> values = new ArrayList<>(stashMap.values());
+		for (Node node : values) if (node.isRoot()) nodes.add(node);
 		return nodes;
 	}
 
