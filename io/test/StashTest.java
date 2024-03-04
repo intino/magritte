99b1adec2d8346a9be53915f1a67c61438c5dd96
Octavio Roncal <octavioroncal11@gmail.com>
2024-03-04 11:14:56 +0000

    updated tests

--- io/test/StashTest.java
+++ io/test/StashTest.java
@@ -1,3 +1,5 @@
+import com.google.gson.*;
+import io.intino.alexandria.Base64;
 import io.intino.alexandria.Json;
 import io.intino.magritte.io.StashDeserializer;
 import io.intino.magritte.io.StashSerializer;
@@ -5,10 +7,7 @@ import io.intino.magritte.io.model.Concept;
 import io.intino.magritte.io.model.Node;
 import io.intino.magritte.io.model.Stash;
 import io.intino.magritte.io.model.Variable;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Ignore;
-import org.junit.Test;
+import org.junit.*;
 
 import java.io.File;
 import java.io.IOException;
@@ -16,10 +15,7 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
-import java.util.Random;
+import java.util.*;
 import java.util.stream.Stream;
 
 import static io.intino.magritte.io.Helper.*;
@@ -57,6 +53,39 @@ public class StashTest {
 		}
 	}
 
+	@Test
+	@Ignore
+	public void serializeStashParts() throws IOException {
+		InputStream stream = StashTest.class.getResourceAsStream("./gestion_cobro_201811.json");
+		String json = new String(stream.readAllBytes(), StandardCharsets.UTF_8);
+		Gson gson = gsonBuilder().create();
+		for (int i = 0; i < 1000; i += 1) {
+			Stash stash = gson.fromJson(json, Stash.class);
+			stash.nodes = new ArrayList<>(stash.nodes.subList(i, i + 2));
+//			stash.nodes.forEach(n -> remove(n.variables, List.of("fecha")));
+			byte[] serialize = StashSerializer.serialize(stash);
+			Stash returnedStash = StashDeserializer.stashFrom(serialize);
+			if (returnedStash == null) {
+				System.err.println(gson.toJson(stash));
+				Assert.assertNotNull("Falló en " + i, returnedStash);
+			}
+		}
+	}
+
+	private static GsonBuilder gsonBuilder() {
+		return new GsonBuilder().setNumberToNumberStrategy(ToNumberPolicy.LONG_OR_DOUBLE).registerTypeAdapter(Number.class, (JsonDeserializer<Number>) (json, type1, context) -> {
+			return json.getAsJsonPrimitive().getAsString().contains(".") ? json.getAsJsonPrimitive().getAsDouble() : json.getAsJsonPrimitive().getAsLong();
+		}).registerTypeAdapter(Number.class, (JsonSerializer<Number>) (json, type1, jsonDeserializationContext) -> new JsonPrimitive(json));
+
+	}
+
+	private void removeValue(List<Variable> variables, String variable, List<String> values) {
+		variables.stream().filter(v -> v.name.equals(variable)).forEach(v -> v.values.removeIf(values::contains));
+	}
+
+	private void remove(List<Variable> variables, List<String> names) {
+		variables.removeIf(v -> names.contains(v.name));
+	}
 
 	@Test
 	public void create_and_read_stash() {
