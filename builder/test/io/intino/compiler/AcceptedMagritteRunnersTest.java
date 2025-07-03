eaf4ccf94ba8d97dc5f554fb503bdbd8ae775bec
Octavio Roncal <octavioroncal@monentia.com>
2025-07-03 11:59:23 +0200

    updated code generation transforming snakeCase to camel

--- builder/test/io/intino/compiler/AcceptedMagritteRunnersTest.java
+++ builder/test/io/intino/compiler/AcceptedMagritteRunnersTest.java
@@ -75,6 +75,12 @@ public class AcceptedMagritteRunnersTest {
 		main(new String[]{temp(home + "sandbox/confFiles/ness/m2.txt")});
 	}
 
+
+	@Test
+	public void visora_m2() {
+		main(new String[]{temp(home + "sandbox/confFiles/visora/m2.txt")});
+	}
+
 	@Test
 	public void ness_m1() {
 		main(new String[]{temp(home + "sandbox/confFiles/ness/m1.txt")});
